ebeb54277c79a448d3f74eb3663fe764b087f7a2
Luiz Nicolat <luiz.nicolat@hotmail.com>
2018-03-07 21:00:54 -0300

    Adicionando Arquivos do Projeto

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function () {
+    jQuery('div.carregatopo').fadeIn(1500);
+    jQuery('div.carregaformacao').fadeIn(2000);
+    jQuery('div.carregaconhecimentos').fadeIn(2500);
+    jQuery('div.carregaexperiencia').fadeIn(3000);
+    
+});
