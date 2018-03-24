e9d277beca9b3a61f47fbf29cdddbb082c68fd9c
Luiz <luiz.nicolat@hotmail.com>
2018-03-24 15:42:12 -0300

    Adicionado botão de imagem

--- indexSite.php
+++ indexSite.php
@@ -8,7 +8,7 @@
 <meta charset="UTF-8">
 
 <link rel="icon" type="image/x-icon" href="./manager.ico" />
-    
+
 <link href="https://fonts.googleapis.com/css?family=Karma" rel="stylesheet">
 
 <!-- Latest compiled and minified CSS -->
@@ -19,14 +19,14 @@
 <link type="text/css" rel="stylesheet" href="estiloindexsite.css">
 <!-- Optional theme -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-  
+
 <!--    Jquery -->
 <script src="https://code.jquery.com/jquery-3.3.1.js"
   integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
   crossorigin="anonymous">
-</script>  
-     
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>    
+</script>
+
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script src="./js/tradPTBR.js"></script>
     <script src="./js/scriptsindexsite.js"></script>
 
@@ -57,6 +57,20 @@
               <input type="text" class="form-control container-fluid" id="codigo" placeholder="01111111" disabled>
             </div>
         </div>
+        <div class="form-group col-md-12 col-xs-12 col-sm-12 col-lg-12">
+          <div >
+            <label for="imgcv">Foto do Currículo</label>
+            <input type="file" name="img[]" class="file" id="imgcv">
+            <div class="input-group col-xs-12">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-picture"></i></span>
+              <input type="text" class="form-control input-lg" disabled placeholder="Escolher imagem...">
+              <span class="input-group-btn">
+                <button class="browse btn btn-default input-lg" type="button"><i class="glyphicon glyphicon-search"></i> Buscar</button>
+              </span>
+            </div>
+          </div>
+
+        </div>
         <div class="form-group col-md-4 col-xs-12 col-lg-4">
             <label for="nome">Nome</label>
             <input type="text" class="form-control" id="nome" placeholder="Nome">
@@ -84,7 +98,7 @@
         <div class="separador container col-md-12 col-xs-12 col-sm-12 col-lg-12">
             <hr class="style1">
         </div>
-        <div id="formacao">
+        <div id="formacao1">
         <div class="container formatfonte col-md-12 col-xs-12 col-sm-12 col-lg-12">
             <h4>Formação</h4>
         </div>
@@ -122,14 +136,14 @@
         <div class="form-group col-md-12 col-xs-12 col-lg-12">
             <label for="textoforma">Descrição da Formação</label>
             <textarea class="form-control" rows="5" id="textoforma" placeholder="Descreva os assuntos abordados durante a formação."></textarea>
-        </div>        
         </div>
+        </div>
+    </form>
         <div class="form-group col-md-12 col-xs-12 col-sm-12 col-lg-12">
             <div class="row col-md-4 col-xs-12 col-lg-4">
             <input type="button" class="btn btn-default" value="Adicionar DIV" id="target">
             </div>
         </div>
-    </form>
   </div>
 </body>
 </html>
--- js/scriptsindexsite.js
+++ js/scriptsindexsite.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
-    
-    
+
+
       jQuery('#datepicker').datepicker({
 //        minDate: '-100Y',
 //        maxDate: '-18Y',
@@ -8,13 +8,13 @@ jQuery(document).ready(function () {
        changeYear: true,
         yearRange: "1900:2019"
       });
-     
+
 
       jQuery.datepicker.setDefaults($.extend(
-          
+
       jQuery.datepicker.regional[ 'pt-BR' ])
     );
-    jQuery( '#datepicker' ).datepicker({        
+    jQuery( '#datepicker' ).datepicker({
       beforeShow: function(input) {
         jQuery( input ).css( 'background-color', '#ff9' );
       },
@@ -25,14 +25,32 @@ jQuery(document).ready(function () {
 //        jQuery( this ).css( 'background-color', '' );
 //      }
     });
-  
+
     var iCnt = 2;
-    $( "#target" ).click(function() {       
-        
-            $('#formacao')
-                .clone().val('')      // CLEAR VALUE.
-                .attr('id', 'formacao' + iCnt)     // GIVE IT AN ID.
-                .appendTo("#formacao");
+    $( "#target" ).click(function() {
+
+      var clonedDiv = $('#formacao1').clone();
+      clonedDiv.attr("id", "formacao" + iCnt);
+      alert("formacao"+iCnt);
+      $('#formacao1').after(clonedDiv);
         iCnt = iCnt+1;
     });
+
+    jQuery(document).on('click', '.browse', function(){
+      var file = jQuery(this).parent().parent().parent().find('.file');
+      file.trigger('click');
+    });
+    jQuery(document).on('change', '.file', function(){
+      jQuery(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+    });
+
+    // var iCnt = 2;
+    // $( "#target" ).click(function() {
+    //
+    //         $('#formacao')
+    //             .clone().val('')      // CLEAR VALUE.
+    //             .attr('id', 'formacao' + iCnt)     // GIVE IT AN ID.
+    //             .appendTo("#formacao");
+    //     iCnt = iCnt+1;
+    // });
 });
