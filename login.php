4c52868faabeb93469461ba5efaed783f18a0b54
WolfHoffman <luiz.nicolat@hotmail.com>
2018-04-21 13:57:30 -0300

    novos

---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Luiz Gustavo Nicolat - Curriculum Vitae</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="The Curriculum Vitae"/>
+<meta charset="UTF-8">
+
+<link rel="icon" type="image/x-icon" href="./manager.ico" />
+
+<link href="https://fonts.googleapis.com/css?family=Karma" rel="stylesheet">
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<link type="text/css" rel="stylesheet" href="./CSS/jquery-ui.css">
+<link type="text/css" rel="stylesheet" href="./CSS/style.css">
+<link type="text/css" rel="stylesheet" href="estiloindexsite.css">
+<link type="text/css" rel="stylesheet" href="./CSS/overhang.css">
+<link type="text/css" rel="stylesheet" href="./CSS/login.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!--    Jquery -->
+<script src="https://code.jquery.com/jquery-3.3.1.js"
+  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+  crossorigin="anonymous">
+</script>
+
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="./js/tradPTBR.js"></script>
+    <script src="./js/scriptsindexsite.js"></script>
+    <script src="./js/buscacep.js"></script>
+    <script src="./js/overhang.js"></script>
+
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body id="top container-fluid">
+  <div class="container">
+    <div class="Absolute-Center is-Responsive">
+      <div id="logo-container"></div>
+      <div class="col-sm-12 col-md-10 col-md-offset-1">
+        <form action="" id="loginForm">
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+            <input class="form-control" type="text" name='username' placeholder="Usuário"/>
+          </div>
+          <div class="form-group input-group">
+            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+            <input class="form-control" type="password" name='password' placeholder="Senha"/>
+          </div>
+          <div class="form-group">
+            <button type="button" class="btn btn-warning btn-block">Login</button>
+          </div>
+          <!-- <div class="form-group text-center">
+            <a href="#">Forgot Password</a>&nbsp;|&nbsp;<a href="#">Support</a>
+          </div> -->
+        </form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
